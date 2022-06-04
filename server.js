855483be225e721d939eba8958e3b3875cdb6876
Davi Fernandes <davifernandessrn@gmail.con>
2022-06-04 20:12:44 -0300

    davi

--- server.js
+++ server.js
@@ -1,22 +1,8 @@
-import app from './src/Controllers/app.js'
-import cors from 'cors';
-import express from "express"; 
-
-
-app.use(express.json());
-
-app.use((req, res, next) =>{
-    res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");  
-    res.header("Access-Control-Allow-Methods", "PATCH, POST, GET, PUT, DELETE, OPTIONS");
-    app.use(cors());
-    next();
-
-});
-
+import app from './src/app.js'
 
 const port = process.env.PORT || 8000;
+
 
 app.listen(port, () => {
     console.log(`servidor escutando em http://localhost:${port}`)
 });
-
--- src/app.js
+++ src/app.js
@@ -1,21 +1,22 @@
 import express from "express"; 
 ////conexão com o banco de dados feita pelo mongoose
-import db from "../config/dbconnect.js"
+import db from "./config/dbconnect.js"
 //importando o arquivo de rotas 
-import routes from "../routes/index.js"
+import routes from "./routes/index.js";
+import cors from "cors";
+
 db.on("error", console.log.bind(console, 'Erro de conexão com banco de dados'))
 db.once("open", () => {
     console.log("Conexão com o banco feita com sucesso")
 })
 
-
 //instancia do express
 const app = express();
 //body-parser usando o express
 app.use(express.json())
-//usando a rota 
-routes(app);
+app.use(cors())
 
+routes(app);
 
 
 export default app;
